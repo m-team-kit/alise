afd04981f6285f7e5a7f63767d3424916c6ca2a6
Marcus <marcus@hardt-it.de>
2024-04-10 11:35:05 +0200

    black

--- alise/oauth2_config.py
+++ alise/oauth2_config.py
@@ -229,7 +229,7 @@ def get_provider_name_by_iss(iss: str) -> str:
 def get_provider_name_by_hash(iss: str, hash_method="sha1") -> str:
     for x in oauth2_config.clients:
         hash_function = getattr(hashlib, hash_method)()
-        hash_function.update((x.backend.OIDC_ENDPOINT+"\n").encode())
+        hash_function.update((x.backend.OIDC_ENDPOINT + "\n").encode())
         hash = hash_function.hexdigest()
         # logger.debug(F"hash: {hash} - {iss} - {x.backend.OIDC_ENDPOINT.encode()}")
         if hash == iss:  # pyright: ignore
