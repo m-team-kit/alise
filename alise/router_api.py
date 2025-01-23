baf553890bb9438166640f40735dffedc587a890
Marcus <marcus@hardt-it.de>
2025-01-23 13:48:00 +0100

    push version

--- alise/router_api.py
+++ alise/router_api.py
@@ -26,7 +26,7 @@ from alise.oauth2_config import get_providers_long
 from alise.models import DatabaseUser
 from alise.config import CONFIG
 
-VERSION = "1.0.5"
+VERSION = "1.0.5-simpl-saml"
 # app = FastAPI()
 flaat = Flaat()
 security = HTTPBearer()
